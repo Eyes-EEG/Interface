6694b94f5ec8c3eab45dd1bfcfbc75b6f913541d
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-09-02 09:09:39 +0100

    Update Manager.cs

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,44 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System;
+using System.IO;
+using System.Text;
 
 public class Manager : MonoBehaviour
 {
     public Boton[] botones;
     public int Posicion = 0;
-    
+
     // Start is called before the first frame update
+    TcpListener listener;
+    String msg;
     void Start()
     {
-        
+        listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 55001);
+        listener.Start();
+        print("is listening");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown (KeyCode.D))
+        if (!listener.Pending())
+        {
+        }
+        else
+        {
+            
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            msg = reader.ReadToEnd();
+            
+        }
+
+        if (msg == "2")
         {
             botones[Posicion].Seleccionado = false;
             Posicion++;
 
-            if(Posicion <0)
+            if (Posicion < 0)
             {
                 Posicion = botones.Length - 1;
                 botones[Posicion].Seleccionado = true;
+                msg = "1";
                 return;
             }
-            if (Posicion > botones.Length -1)
+            if (Posicion > botones.Length - 1)
             {
                 Posicion = 0;
                 botones[Posicion].Seleccionado = true;
+                msg = "1";
                 return;
             }
 
-            botones [Posicion].Seleccionado = true;
+            botones[Posicion].Seleccionado = true;
+            msg = "1";
 
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (msg == "3")
         {
             botones[Posicion].Seleccionado = false;
             Posicion--;
@@ -47,42 +72,45 @@ public class Manager : MonoBehaviour
             {
                 Posicion = botones.Length - 1;
                 botones[Posicion].Seleccionado = true;
+                msg = "1";
                 return;
             }
             if (Posicion > botones.Length - 1)
             {
                 Posicion = 0;
                 botones[Posicion].Seleccionado = true;
+                msg = "1";
                 return;
             }
 
-            botones [Posicion].Seleccionado = true;
+            botones[Posicion].Seleccionado = true;
+            msg = "1";
         }
 
-        if (botones[0].Seleccionado && Input.GetKeyDown(KeyCode.Space))
+        if (botones[0].Seleccionado && msg == "4")
         {
-            print("arriba");
-            
+            print("Arriba");
+            msg = "1";
         }
 
-        if (botones[1].Seleccionado && Input.GetKeyDown(KeyCode.Space))
+        if (botones[1].Seleccionado && msg == "4")
         {
-            print("izquierda");
-
+            print("Izquierda");
+            msg = "1";
         }
 
-        if (botones[2].Seleccionado && Input.GetKeyDown(KeyCode.Space))
+        if (botones[2].Seleccionado && msg == "4")
         {
-            print("abajo");
-
+            print("Abajo");
+            msg = "1";
         }
 
-        if (botones[3].Seleccionado && Input.GetKeyDown(KeyCode.Space))
+        if (botones[3].Seleccionado && msg == "4")
         {
-            print("derecha");
-
+            print("Derecha");
+            msg = "01";
         }
-    }  
+    }
 
 
 }
